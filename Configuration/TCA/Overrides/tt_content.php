46bc264ad3f970060cb091256c402bca850986b6
Andre Kraus <info@andrekraus.com>
2025-08-07 14:11:02 +0200

    [SLIDE] Extend TCA

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,3 @@
+<?php
+$GLOBALS['TCA']['tt_content']['columns']['example_text_modified_field']['description'] = 'NO this is not modified - joking! ;)';
+$GLOBALS['TCA']['tt_content']['columns']['example_text_modified_field']['config']['enableRichtext'] = 1;
